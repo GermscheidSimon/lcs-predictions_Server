d032f4f7cd3aa8f06285e3ca3bcaebd7ec793cd7
Simon Germscheid <sgermscheid@gmail.com>
2021-02-01 11:22:19 -0600

    server route setup for schedules GET

--- server/routes/schedule.router.js
+++ server/routes/schedule.router.js
@@ -4,17 +4,20 @@ const router = express.Router()
 const axios = require('axios')
 
 
-router.get('/fetchSchedules', (req, res) => {
+router.get('/fetchSchedules', async (req, res) => {
+
+    console.log('schedule fetch');
     try {
 
-        axios({
+       const schedule = await axios({
             method: 'GET',
             url: 'https://esports-api.lolesports.com/persisted/gw/getSchedule?hl=en-US&leagueId=98767991299243165',
             headers: {
                 "x-api-key": `${process.env.API_KEY}`
             }
-        })
-
+        });
+        console.log(schedule.data);
+        res.send(schedule.data);
     } catch (error) {
         console.log(error);
     }
