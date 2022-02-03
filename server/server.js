73539f8343beab5c80ca1364edb1b7d8788508f3
Simon Germscheid <67908776+Khaildoran@users.noreply.github.com>
2022-02-03 17:35:25 -0600

    update scripting for heroku

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require('dotenv').config();
 const app = express()
-const PORT = 5000 || process.env.PORT
+const PORT = process.env.PORT || 5000
 
 const bodyParser = require('body-parser');
 const sessionMiddleware = require('./modules/session-middleware');
