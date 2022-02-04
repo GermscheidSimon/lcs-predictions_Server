776883bd8a0680f3a46428fdfdfc0492d636f62b
Simon Germscheid <67908776+Khaildoran@users.noreply.github.com>
2022-02-03 20:59:46 -0600

    added cors

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 require('dotenv').config();
+var cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use(cors())
 const bodyParser = require('body-parser');
 const sessionMiddleware = require('./modules/session-middleware');
 const passport = require('./modules/userStrategy');
